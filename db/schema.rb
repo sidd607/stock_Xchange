dc4e892094ef0585d458e2eabaa9beea690dba33
Raghav Bhatnagar <raghavbhatnagar96@gmail.com>
2016-02-02 02:12:59 +0530

    Linked portfolio with transaction. Made change in transaction schema. Buy_id becomes portfolio_id. Making a new portfolio or selling stocks automatically creates new transaction. Removed functions like destroy, edit for transactions which function as logs.
    Signed-off-by: Gaurav Koley <arkokoley@live.in>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160129142646) do
+ActiveRecord::Schema.define(version: 20160201202542) do
 
   create_table "colleges", force: :cascade do |t|
     t.string   "name",       limit: 255, null: false
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 20160129142646) do
     t.integer  "transaction_type", limit: 2,  null: false
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
-    t.integer  "buy_id",           limit: 4
+    t.integer  "portfolio_id",     limit: 4
   end
 
   create_table "users", force: :cascade do |t|
