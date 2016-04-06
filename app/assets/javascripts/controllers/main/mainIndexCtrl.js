2360be02b3c387bb681bcc71259df255579156ca
Suprgya Bhushan <suprgya.56@iiitb.org>
2016-04-07 00:18:23 +0530

    Remove useless files from app/assets

--- app/assets/javascripts/controllers/main/mainIndexCtrl.js
+++
@@ -1,4 +0,0 @@
-angular.module('stockXchange')
-	.controller('IndexCtrl', function($scope){
-		$scope.title = "my blog";
-	})
--- app/assets/javascripts/ng-app/controllers/index.js
+++
@@ -1,4 +0,0 @@
-angular.module('stockXchange')
-    .controller('IndexCtrl', function ($scope) {
-        $scope.things = ['Angular', 'Rails 4.1', 'Working', 'Together!!'];
-    });
