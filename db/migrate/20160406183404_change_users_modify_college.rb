d8612181ac884ab311275bc6846bf32f96ca5dbb
viky <vikas.yadav@iiitb.org>
2016-04-08 15:43:52 +0530

    Fixes migration issues

--- db/migrate/20160406183404_change_users_modify_college.rb
+++ db/migrate/20160406183404_change_users_modify_college.rb
@@ -1,6 +1,6 @@
 class ChangeUsersModifyCollege < ActiveRecord::Migration
   def change
     remove_column :users, :college
-    add_column :users, :college_id
+    add_column :users, :college_id, :integer
   end
 end
