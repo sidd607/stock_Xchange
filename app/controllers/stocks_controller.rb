b37e3bb88379e2e77538248890fc9df2ea7578c5
sidd607 <siddartha.padhi@gmail.com>
2016-04-22 18:59:58 +0530

    sell

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -30,6 +30,23 @@ class StocksController < ApplicationController
     render :json => JSON.parse(result)
   end
 
+  def sell
+    stockNumber = params[:stock]["stock_count"]
+    result = current_user.sell(params[:id], stockNumber)
+    if result == true
+      result = '{"result": "successfull}'
+    else
+      tmp = result
+      result = '{"result": "failed", "error": "' + tmp + '"}'
+    end
+    render :json => JSON.parse(result)
+  end
+
+  def nse
+    @stocks = Stock.where(stock_type: 1)
+  end
+
+
   private
     def stock_params
       params.require(:stock).permit(:code,:name,:stock_type,:current_price,:opening_price,:day_high,:day_low,:sold_count,:created_at,:updated_at)
